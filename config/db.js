ebfd368b91a0c4711188bd98258183e0970b4f47
Gudmer Granados <gc.gudmer@gmail.com>
2025-07-05 16:12:38 -0500

    Actualizo configuración de conexión a la BD

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PASSWORD, // <--- este cambio
+  process.env.DB_PASS, // <--- este cambio
   {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 3306,
