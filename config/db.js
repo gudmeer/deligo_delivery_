d9baead3329a5ae527f78d78d21dfe2a0e6ce178
Gudmer Granados <gc.gudmer@gmail.com>
2025-07-07 11:43:23 -0500

    Descripción de los cambios

--- config/db.js
+++ config/db.js
@@ -1,12 +1,8 @@
 // config/db.js
-const { Sequelize } = require('sequelize');
-const dotenv = require('dotenv');
-dotenv.config();
-
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PASS, // <--- este cambio
+  process.env.DB_PASSWORD, // <-- aquí el cambio
   {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 3306,
